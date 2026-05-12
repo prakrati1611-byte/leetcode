56bb83bd3acb7bb658f4d190b6b44897cb393bc2
Prakrati Dubey <prakrati1611@gmail.com>
2026-05-12 06:37:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (37.33%)

---
+++ leetcode-solutions/0268-missing-number/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        expected = n * (n + 1) // 2
+        return expected - sum(nums)
+        
