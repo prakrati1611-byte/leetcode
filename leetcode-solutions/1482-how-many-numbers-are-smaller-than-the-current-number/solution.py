f97d048e6ef2699b98725976500effdda55fadc7
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-09 11:13:07 +0000

    Sync LeetCode submission Runtime - 130 ms (50.37%), Memory - 12.4 MB (45.96%)

---
+++ leetcode-solutions/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        ans=[]
+        for i in nums:
+            c=0
+            for j in nums:
+                
+                if j<i:
+                    c+=1
+            ans.append(c)
+        return ans
+
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
