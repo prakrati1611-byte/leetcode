8d0ccb82ce2fda221f00e7fee3b49fc4758842a0
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-11 14:58:47 +0000

    Sync LeetCode submission Runtime - 671 ms (12.21%), Memory - 12.4 MB (55.78%)

---
+++ leetcode-solutions/1013-fibonacci-number/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def fib(self, n):
+        if n==0 or n==1:
+            return n
+        return self.fib(n-1)+self.fib(n-2)
+
+        print(fib(n))
+        """
+        :type n: int
+        :rtype: int
+        """
+        
