c5790ea78cbadaa40c1e03498a39aaae8b7adb79
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-11 17:27:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (55.26%)

---
+++ leetcode-solutions/0231-power-of-two/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n<=0:
+            return False
+        if n==1:
+            return True
+        if n%2!=0:
+            return False
+        
+        return self.isPowerOfTwo(n//2)
