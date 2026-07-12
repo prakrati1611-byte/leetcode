05296036f577e09b6613a8fdb7ac9bcc907bebca
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-12 18:17:01 +0000

    Sync LeetCode submission Runtime - 32 ms (92.77%), Memory - 21.2 MB (44.98%)

---
+++ leetcode-solutions/0053-maximum-subarray/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        curr_sum=0
+        max_sum=nums[0]
+        for i in range(len(nums)):
+            curr_sum+=nums[i]
+            if curr_sum>max_sum:
+                max_sum=curr_sum
+            if curr_sum<0:
+                curr_sum=0
+        return max_sum
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
