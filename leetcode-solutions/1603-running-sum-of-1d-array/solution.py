d89070f66a2d33a14be1c7e0fdfdc74ce80bc69e
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-12 14:03:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.4 MB (96.91%)

---
+++ leetcode-solutions/1603-running-sum-of-1d-array/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def runningSum(self, nums):
+        n=len(nums)
+        ans=[]
+        ans.append(nums[0])
+
+        for i in range(1,n):
+            x=ans[i-1]+nums[i]
+            ans.append(x)
+
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
