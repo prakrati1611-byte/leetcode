46f6316c7ad1c897ef895651f2b486ee14efb58a
Prakrati Dubey <prakrati1611@gmail.com>
2026-07-12 18:26:57 +0000

    Sync LeetCode submission Runtime - 108 ms (40.69%), Memory - 19.4 MB (14.37%)

---
+++ leetcode-solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price=prices[0]
+        profit=0
+
+        for i in range(1,len(prices)):
+            curr_profit=prices[i]-min_price
+            if curr_profit>profit:
+                profit=curr_profit
+            min_price=min(min_price,prices[i])
+        return profit
+
+
